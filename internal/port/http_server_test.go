880af8777ed8692447f342c39053bd235157a800
Trevor Atkinson <trevor@badappsdevelopment.com>
2023-06-28 01:38:27 -0600

    17 cicd unit testing (#18)
    * update ci/cd pipeline
    
    * fix unit tests
    
    ---------
    
    Co-authored-by: trevor atkinson <trevor@badappsdevelompent.com>

--- internal/port/http_server_test.go
+++ internal/port/http_server_test.go
@@ -2,7 +2,12 @@ package port_test
 
 import (
 	"bytes"
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/x509"
 	"encoding/json"
+	"encoding/pem"
 	"net/http"
 	"net/http/httptest"
 	"os"
@@ -20,7 +25,7 @@ import (
 func init() {
 	_ = os.Setenv("SQLITE_DSN", "testfiles/db/chat.db")
 	_ = os.Setenv("SQLITE_MIGRATIONS_DIR", "../../migrations")
-	_ = os.Setenv("JWT_PRIVATE_KEY", "testfiles/certs/ec-secp384r1-priv-key.der")
+	_ = os.Setenv("JWT_PRIVATE_KEY", "testfiles/certs/privateKey.der")
 }
 
 type HTTPServerSuite struct {
@@ -31,6 +36,28 @@ type HTTPServerSuite struct {
 func (s *HTTPServerSuite) SetupTest() {
 
 	a := assert.New(s.T())
+
+	_ = os.Mkdir("testfiles", os.ModePerm)
+
+	_ = os.Mkdir("testfiles/certs", os.ModePerm)
+
+	_ = os.Mkdir("testfiles/db", os.ModePerm)
+
+	f, e := os.Create("testfiles/db/chat.db")
+	a.NoError(e)
+	_ = f.Close()
+
+	pk, e := ecdsa.GenerateKey(elliptic.P384(), rand.Reader)
+	a.NoError(e)
+
+	pb, e := x509.MarshalECPrivateKey(pk)
+	a.NoError(e)
+
+	bb := pem.EncodeToMemory(&pem.Block{Type: "PRIVATE KEY", Bytes: pb})
+	a.NoError(e)
+
+	e = os.WriteFile("testfiles/certs/privateKey.der", bb, 0600)
+	a.NoError(e)
 
 	sdb, e := db.NewSQLite()
 	a.NoError(e)
@@ -49,48 +76,27 @@ func (s *HTTPServerSuite) SetupTest() {
 	s.mux = port.NewRouter(srv, mw)
 }
 
-func (s *HTTPServerSuite) TestCreateUser() {
-
-	a := assert.New(s.T())
-
-	cases := []struct {
-		expected int
-		payload  *port.NewUserParams
-	}{
-		{
-			// success
-			expected: http.StatusCreated,
-			payload: &port.NewUserParams{
-				NewUserPayload: &port.NewUserPayload{
-					Username: "john.doe",
-					Email:    "john.doe@mailbox.com",
-					Password: "test123",
-				},
-			},
-		},
-	}
-
-	for _, c := range cases {
-
-		bb, e := json.Marshal(c.payload)
-		a.NoError(e)
-
-		rq, e := http.NewRequest(http.MethodPost, "/api/v1/user", bytes.NewReader(bb))
-		a.NoError(e)
-
-		rq.Header.Add("Content-Type", "application/json")
-
-		rr := httptest.NewRecorder()
-
-		s.mux.ServeHTTP(rr, rq)
-
-		a.Equal(c.expected, rr.Code)
-	}
-}
-
 func (s *HTTPServerSuite) TestUserLogin() {
 
 	a := assert.New(s.T())
+
+	bb, e := json.Marshal(&port.NewUserParams{
+		NewUserPayload: &port.NewUserPayload{
+			Username: "john.doe",
+			Email:    "john.doe@mailbox.com",
+			Password: "test123",
+		},
+	})
+	a.NoError(e)
+
+	rq, e := http.NewRequest(http.MethodPost, "/api/v1/user", bytes.NewReader(bb))
+	a.NoError(e)
+
+	rq.Header.Add("Content-Type", "application/json")
+
+	rr := httptest.NewRecorder()
+
+	s.mux.ServeHTTP(rr, rq)
 
 	cases := []struct {
 		expected int
@@ -120,6 +126,45 @@ func (s *HTTPServerSuite) TestUserLogin() {
 		a.NoError(e)
 
 		rq, e := http.NewRequest(http.MethodPost, "/api/v1/user/login", bytes.NewReader(bb))
+		a.NoError(e)
+
+		rq.Header.Add("Content-Type", "application/json")
+
+		rr := httptest.NewRecorder()
+
+		s.mux.ServeHTTP(rr, rq)
+
+		a.Equal(c.expected, rr.Code)
+	}
+}
+
+func (s *HTTPServerSuite) TestCreateUser() {
+
+	a := assert.New(s.T())
+
+	cases := []struct {
+		expected int
+		payload  *port.NewUserParams
+	}{
+		{
+			// success
+			expected: http.StatusCreated,
+			payload: &port.NewUserParams{
+				NewUserPayload: &port.NewUserPayload{
+					Username: "john.doe",
+					Email:    "john.doe@mailbox.com",
+					Password: "test123",
+				},
+			},
+		},
+	}
+
+	for _, c := range cases {
+
+		bb, e := json.Marshal(c.payload)
+		a.NoError(e)
+
+		rq, e := http.NewRequest(http.MethodPost, "/api/v1/user", bytes.NewReader(bb))
 		a.NoError(e)
 
 		rq.Header.Add("Content-Type", "application/json")
